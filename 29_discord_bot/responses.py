52b6ed7a81d1f3d5e0eebb6d4ef0a46a0e972d88
Olisamedua Okafor <olisa.okafor@outlook.com>
2024-06-08 23:07:51 -0400

    commit

--- 29_discord_bot/responses.py
+++ 29_discord_bot/responses.py
@@ -1,24 +1,26 @@
 from difflib import get_close_matches
 import json
+from typing import Optional
 
 
-def get_best_match(user_question: str, questions: dict) -> str | None:
+def get_best_match(user_question: str, questions: dict) -> Optional[str]:
     """Compares the user message similarity to the ones in the dictionary"""
 
-    questions: list[str] = [q for q in questions]
-    matches: list = get_close_matches(user_question, questions, n=1, cutoff=0.6)
+    questions_list = [q for q in questions]
+    matches = get_close_matches(user_question, questions_list, n=1, cutoff=0.6)
 
     # Return the first best match, else return None
     if matches:
         return matches[0]
+    return None
 
 
 def get_response(message: str, knowledge: dict) -> str:
     # Finds the best match, otherwise returns None
-    best_match: str | None = get_best_match(message, knowledge)
+    best_match: Optional[str] = get_best_match(message, knowledge)
 
     # Gets the best match from the knowledge base
-    if answer := knowledge.get(best_match):
+    if best_match and (answer := knowledge.get(best_match)):
         return answer
     else:
         return 'I don\'t understand... Could you try rephrasing that?'
@@ -27,8 +29,3 @@ def get_response(message: str, knowledge: dict) -> str:
 def load_knowledge(file: str) -> dict:
     with open(file, 'r') as f:
         return json.load(f)
-
-# if __name__ == "__main__":
-#     test_knowledge: dict = load_knowledge('knowledge.json')
-#     test_response: str = get_response('hello', knowledge=test_knowledge)
-#     print(test_response)
